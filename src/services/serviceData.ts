b1b034405cda01ca3adcae4ea740efa8733ce4cc
thisdev-davi <davisouzaoliveira1975@gmail.com>
2025-09-14 16:10:07 -0300

    new function buscarDados

--- src/services/serviceData.ts
+++ src/services/serviceData.ts
@@ -6,19 +6,25 @@ const URL_CLIENTES = "https://docs.google.com/spreadsheets/d/1PBN_HQOi5ZpKDd63mo
 const URL_CONTAS = "https://docs.google.com/spreadsheets/d/1PBN_HQOi5ZpKDd63mouxttFvvCwtmY97Tb5if5_cdBA/gviz/tq?tqx=out:csv&sheet=contas";
 const URL_AGENCIAS = "https://docs.google.com/spreadsheets/d/1PBN_HQOi5ZpKDd63mouxttFvvCwtmY97Tb5if5_cdBA/gviz/tq?tqx=out:csv&sheet=agencias";
 
-export const buscarClientes = async(): Promise<Cliente[]> => {
-    const response = await fetch(URL_CLIENTES);
+async function  buscarDados<T>(url: string, transformador: (item:any)=> T): Promise<T[]> {
+    const response = await fetch(url);
     const textCsv = await response.text();
     const parsedData = Papa.parse(textCsv, {
         header: true, // primeira linha é cabecalho
         dynamicTyping: true, // tenta adivinhar o tipo do dado
         skipEmptyLines: true,
-    })
-    console.log("Debug Papaparse [CLIENTES]:", parsedData);
+    });
 
-    const clientes: Cliente[] = parsedData.data.map((item:any) => ({
-        id: item.id.toString(),
-        cpfCnpj: item.cpfCnpj,
+    const dadosLimpos = parsedData.data.filter((item: any): 
+    item is Record<string, any> => item && item.id).map(transformador);
+
+    return dadosLimpos;
+}
+
+export const buscarClientes = async(): Promise<Cliente[]> => {
+    return buscarDados(URL_CLIENTES, (item) => ({
+        id: String(item.id),
+        cpfCnpj: String(item.cpfCnpj),
         rg: item.rg,
         dataNascimento: new Date(item.dataNascimento), // converter par aum objeto do tipo date
         nome: item.nome,
@@ -30,45 +36,24 @@ export const buscarClientes = async(): Promise<Cliente[]> => {
         estadoCivil: item.estadoCivil,
         codigoAgencia: item.codigoAgencia
     }));
-    return clientes;
 };
 
 export const buscarContas = async(): Promise<Conta[]> => {
-    const response = await fetch(URL_CONTAS);
-    const textCsv = await response.text();
-    const parsedData = Papa.parse(textCsv, {
-        header: true,
-        dynamicTyping: true,
-        skipEmptyLines: true,
-    });
-    console.log("Debug Papaparse [CONTAS]:", parsedData);
-
-    const contas: Conta[] = parsedData.data.map((item:any) => ({
-        id: item.id.toString(),
-        cpfCnpjCliente: item.cpfCnpjCliente,
+    return buscarDados(URL_CONTAS, (item) => ({
+        id: String(item.id),
+        cpfCnpjCliente: String(item.cpfCnpjCliente),
         tipo: item.tipo,
         saldo: item.saldo,
         limiteCredito: item.limiteCredito,
         creditoDisponivel: item.creditoDisponivel,
-    }));
-    return contas;
+    }))
 };
 
 export const buscarAgencias = async(): Promise<Agencia[]> => {
-    const response = await fetch(URL_AGENCIAS);
-    const textCsv = await response.text();
-    const parsedData = Papa.parse(textCsv, {
-        header: true,
-        dynamicTyping: true,
-        skipEmptyLines: true,
-    });
-    console.log("Debug Papaparse [AGENCIAS]:", parsedData);
-
-    const agencias: Agencia[] = parsedData.data.map((item:any) => ({
-        id: item.id.toString(),
+    return buscarDados(URL_AGENCIAS, (item) => ({
+        id: String(item.id),
         codigo: item.codigo,
         nome: item.nome,
         endereco: item.endereco,
     }));
-    return agencias;
 };
