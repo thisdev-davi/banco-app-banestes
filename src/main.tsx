d21bcbf6a2928f6a599d0e8eb3f3f36d038b4077
thisdev-davi <davisouzaoliveira1975@gmail.com>
2025-09-14 21:18:35 -0300

    new routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import './index.css'
 import App from './App.tsx'
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ClientDetailPage } from "./pages/ClientDetailPage/index.tsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>
   },
+
+  {
+    path: "/cliente/:id",
+    element: <ClientDetailPage/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
