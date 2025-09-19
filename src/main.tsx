f8afe6311ade1a7151e29f34d433a26de40e70ee
thisdev-davi <davisouzaoliveira1975@gmail.com>
2025-09-19 11:38:04 -0300

    root

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-
 import { Layout } from "./components/Layout";
 import { ClientListPage } from "./pages/ClientListPage";
 import { ClientDetailPage } from "./pages/ClientDetailPage";
 
-const roteador = createBrowserRouter([
+const roteador = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children:
+      [
+        { 
+          path: "/",
+          element: <ClientListPage />
+        },
+        { path: "/cliente/:id",
+          element: <ClientDetailPage />
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <ClientListPage />,
-      },
-      {
-        path: "/cliente/:id",
-        element: <ClientDetailPage />,
-      },
-    ]
-  },
-]);
+    basename: "/banco-app-banestes/",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
